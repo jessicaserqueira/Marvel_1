4e8a980057d3f32ef96657ea6a1d21dd9eb9d23f
Jessica Serqueira <jessicaserqueira@Mac-mini-de-ioasys.local>
2023-02-08 14:41:30 -0300

    feat: Subindo branche para o repositório remoto

---
+++ Marvel_1/Features/Comics/Sources/Comics/Hq/HQCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Jessica Serqueira on 08/02/23.
+//
+
+import Foundation
---
+++ Marvel_1/Features/Comics/Sources/Comics/Hq/HQListView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jessica Serqueira on 08/02/23.
+//
+
+import Foundation
