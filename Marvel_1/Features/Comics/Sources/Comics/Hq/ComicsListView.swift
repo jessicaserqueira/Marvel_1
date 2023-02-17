eda59a04766d78399c5bc531b20172b357469870
Nathasha <nathashaamaral@gmail.com>
2023-02-17 17:29:40 -0300

    fix screen HQ and details

--- Marvel_1/Features/Comics/Sources/Comics/Hq/ComicsListView.swift
+++ Marvel_1/Features/Comics/Sources/Comics/Hq/ComicsListView.swift
@@ -34,18 +34,23 @@ struct ComicsListView<ViewModel: ComicsModelling>: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
                 ForEach(self.viewModel.filterComics(searchTerm: self.searchTerm), id: \.uniqueId) { i in
+                    
                     ZStack {
                         Rectangle()
                             .foregroundColor(Color.white)
                             .frame(height: comicContainerHeight)
                             .overlay(borderStroke)
-                        HStack (alignment: .top)  {
-                            KFImage(URL(string: "\(i.thumbnail)"))
-                                .resizable()
-                                .frame(width: comicImageWidth, height: comicImageHeight, alignment: .leading)
-                                .foregroundColor(Color.white)
-                                .overlay(borderStroke)
-                                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+                        
+                        HStack (alignment: .top, content: {
+                            Button(action: {
+                            }) {
+                                KFImage(URL(string: "\(i.thumbnail)"))
+                                    .resizable()
+                                    .foregroundColor(Color.white)
+                                    .frame(width: 140, height: 182, alignment: .leading)
+                                    .overlay(borderStroke)
+                                    .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+                            }
                             
                             VStack {
                                 HStack {
@@ -57,19 +62,20 @@ struct ComicsListView<ViewModel: ComicsModelling>: View {
                                         Text(i.title)
                                             .font(Font.custom("Bangers-Regular", size: 12))
                                     }
-                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 15))
+                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8))
                                 }
                                 Text(i.textObjects.first ?? "")
                                     .font(Font.custom("Nunito-Regular", size: 12))
                                     .foregroundColor(.primary)
                                     .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+                                
                                     .lineLimit(8)
                                     .multilineTextAlignment(.leading)
                                     .truncationMode(.tail)
                             }
-                        }
-                        .padding(6)
+                        })
                     }
+                    .padding(.top, 32)
                     .padding(.top, 32)
                 }
             }.padding(.leading, 24)
