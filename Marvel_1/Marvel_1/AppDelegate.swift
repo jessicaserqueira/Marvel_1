cfc4cffca70389b564eea0f389cc9635f8feed75
Nathasha <nathashaamaral@gmail.com>
2023-03-07 11:06:33 -0300

    test fireBase

--- Marvel_1/Marvel_1/AppDelegate.swift
+++ Marvel_1/Marvel_1/AppDelegate.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 import DI
+import Firebase
+import FirebaseFirestore
+import Comics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        FirebaseApp.configure()
         
         return true
     }
@@ -23,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
