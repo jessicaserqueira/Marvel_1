29b03999674e6399204a6be8015486830b71d7c4
Jessica Serqueira <jessicaserqueira@Mac-mini-de-ioasys.local>
2023-03-15 09:43:33 -0300

    feat: configuração do package fireStorage

--- Marvel_1/Core/FireStorage/Package.swift
+++ Marvel_1/Core/FireStorage/Package.swift
@@ -5,22 +5,55 @@ import PackageDescription
 
 let package = Package(
     name: "FireStorage",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FireStorage",
             targets: ["FireStorage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "AppData",
+            path: "../../AppData"
+        ),
+        .package(
+            name: "Common",
+            path: "../../Common"
+        ),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk",
+                 from: "9.6.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FireStorage",
-            dependencies: []),
+            dependencies: [
+                "AppData",
+                "Common",
+                .product(
+                    name: "FirebaseAnalytics",
+                    package: "firebase-ios-sdk"
+                ),
+                .product(
+                    name: "FirebaseRemoteConfig",
+                    package: "firebase-ios-sdk"
+                ),
+                .product(
+                    name: "FirebasePerformance",
+                    package: "firebase-ios-sdk"
+                ),
+                .product(
+                    name: "FirebaseFirestore",
+                    package: "firebase-ios-sdk"
+                ),
+                .product(
+                    name: "FirebaseFirestoreSwift",
+                    package: "firebase-ios-sdk"
+                ),
+                .product(
+                    name: "FirebaseAuth",
+                    package: "firebase-ios-sdk"
+                )
+            ]),
         .testTarget(
             name: "FireStorageTests",
             dependencies: ["FireStorage"]),
