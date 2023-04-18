a7b7663b14d0266103ff39c0524e1f3b48559ec1
Jessica Serqueira <jessicaserqueira@MacBook-Pro-de-Glaycon.local>
2023-04-18 13:55:40 -0300

    refatoração login

--- Marvel_1/Features/Comics/Sources/Comics/Components/LoginTextField.swift
+++ Marvel_1/Features/Comics/Sources/Comics/Components/LoginTextField.swift
@@ -33,8 +33,8 @@ public struct LoginTextField: View {
         
         }
         .padding(12)
-        .frame(width: 327,height: 48)
-        .background(Color.white.opacity(0.2))
+        .frame(height: 48)
+        .background(Color.white.opacity(0.4))
         .overlay(RoundedRectangle(cornerRadius: 10).stroke(borderColor, lineWidth: 2))
     }
 }
